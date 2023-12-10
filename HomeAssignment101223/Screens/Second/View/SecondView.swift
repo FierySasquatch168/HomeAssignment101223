190b2a7ffa11a62c257f0b9ecb385dc1e41c9414
Aleksandr Eliseev <9120603@gmail.com>
2023-12-10 14:47:16 +0200

    swiftUIView added

--- HomeAssignment101223/Screens/Second/View/SecondView.swift
+++ HomeAssignment101223/Screens/Second/View/SecondView.swift
@@ -8,32 +8,48 @@
 import SwiftUI
 
 struct SecondView: View {
-    @StateObject var viewModel: LocationViewModel
-
-        var body: some View {
-            VStack {
+    @ObservedObject var viewModel: LocationViewModel
+    
+    var body: some View {
+        VStack {
+            HStack {
                 Text(viewModel.locationModel.hebrewName)
                     .font(.title)
-                    .padding()
+                Spacer().frame(width: 20)
                 Text(viewModel.locationModel.englishName)
-                    .font(.subheadline)
-                    .padding()
-                Text(viewModel.locationModel.region)
-                    .font(.subheadline)
-                    .padding()
-
-                LikeButton(viewModel: viewModel)
-                    .padding()
-
-                Spacer()
+                    .font(.title)
             }
-            .background(viewModel.isLiked ? Color.pink : Color.white)
-            .onTapGesture {
+            .padding([.top, .leading, .trailing], 40)
+            // Region
+            Text(viewModel.locationModel.region)
+                .font(.headline)
+                .padding([.leading, .trailing])
+            Spacer().frame(height: 100)
+            // Heart button
+            Button(action: {
                 viewModel.toggleLike()
+            }) {
+                Image(systemName: viewModel.isLiked ? "heart.fill" : "heart")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .foregroundColor(.black)
             }
+            .padding()
         }
+        .padding([.top, .leading, .trailing], 20)
+        .background(viewModel.isLiked ? Color.pink : Color.white)
+        .onTapGesture {
+            viewModel.toggleLike()
+        }
+    }
 }
 
-//#Preview {
-//    SecondView(viewModel: <#LocationViewModel#>)
-//}
+#Preview {
+    SecondView(viewModel: .init(locationModel: LocationVisibleModel(
+        location: Location(
+            hebrewName: "לא רשום",
+            englishName: "Mock city",
+            region: "לא ידוע",
+            regionalCouncil: nil),
+        urlForImage: nil)))
+}
